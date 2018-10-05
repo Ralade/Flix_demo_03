ab602e22b79d5cf836968326719f7eb16380bc5c
Temi Alade <temi@temis-macbook-air.local>
2018-10-04 20:31:38 -0400

    Initial Commit

---
+++ Flix_Demo_03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_Demo_03
+//
+//  Created by Temi Alade on 10/4/18.
+//  Copyright © 2018 Rasheedat Alade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
